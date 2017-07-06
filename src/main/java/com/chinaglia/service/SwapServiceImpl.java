5cda255ca1323bf4dc9f5edb158371f2e20b6411
Derek <derekmcusack@gmail.com>
2017-07-06 15:42:53 +0100

    email now saving with swap requests

--- src/main/java/com/chinaglia/service/SwapServiceImpl.java
+++ src/main/java/com/chinaglia/service/SwapServiceImpl.java
@@ -1,6 +1,8 @@
 package com.chinaglia.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.chinaglia.model.SwapOrig;
@@ -14,6 +16,9 @@ public class SwapServiceImpl implements SwapService{
 	
 	@Override
 	public void saveSwap(SwapOrig swapRequest){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String email = auth.getName();
+		swapRequest.setEmail(email);
 		swapRepository.save(swapRequest);
 	}
 
